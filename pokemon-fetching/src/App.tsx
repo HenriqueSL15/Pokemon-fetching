2dfb8635afccdfc08707d4d6f8090befaaeb0199
Henrique <henrynyyls@gmail.com>
2024-02-13 21:10:59 -0300

    Now it shows the name and the weight of the pokemon you searched for

--- pokemon-fetching/src/App.tsx
+++ pokemon-fetching/src/App.tsx
@@ -1,28 +1,35 @@
+import { FireExtinguisher } from "lucide-react";
 import { Button } from "./components/ui/button"
 import { Input } from "./components/ui/input"
 import { useState, useEffect, useRef } from "react";
 
 function App() {
   const [text, setText] = useState('');
+  const [name, setName] = useState('');
+  const [weight, setWeight] = useState('');
+
+  let wholeName = '';
+  let firstCharacter = name.charAt(0).toUpperCase();
+  let restOfName = name.slice(1);
   let [imageUrl, setImageUrl] = useState('');
   const renderCount = useRef(0);
   
   useEffect(() => {
     renderCount.current +=1;
     console.log(imageUrl)
+    console.log(firstCharacter + restOfName)
   })
-
-  const style = {
-    display: 'block'
-  }
 
   function fetchData(){
     fetch(`https://pokeapi.co/api/v2/pokemon/${text.toLowerCase()}`)
       .then(response => response.json())
-      .then(data => setImageUrl(data.sprites.front_default))
+      .then(data => {
+        setImageUrl(data.sprites.front_default)
+        setName(data.name)
+        setWeight(data.weight)
+      })
       .catch(error => console.log(error));
   }
-
 
   return (
     <>
@@ -35,7 +42,11 @@ function App() {
       </div>
 
       <div className="flex justify-center">
-        <img src={imageUrl}id="pokemonImage" style={style} className="border rounded min-w-64 min-h-64 max-w-64"/>
+        <img src={imageUrl}id="pokemonImage" className="border rounded min-w-64 min-h-64 max-w-64"/>
+        <div className="pl-5">
+          <h1>Name: {firstCharacter + restOfName}</h1>
+          <h1>Weight: {weight}</h1>
+        </div>
       </div>
     </div>
     <script type="module" src="./src/script.ts"></script>
