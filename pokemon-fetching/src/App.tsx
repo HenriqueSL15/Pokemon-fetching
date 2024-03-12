a95431e7468c9f0f4643ceaddd64a1cd2f2f3de9
Henrique <henrynyyls@gmail.com>
2024-03-12 16:13:12 -0300

    Now, everything is working

--- pokemon-fetching/src/App.tsx
+++ pokemon-fetching/src/App.tsx
@@ -1,154 +1,100 @@
-import {
-  navigationMenuTriggerStyle,
-  NavigationMenu,
-  NavigationMenuList,
-  NavigationMenuItem,
-  NavigationMenuContent,
-  NavigationMenuTrigger,
-  NavigationMenuLink,
-  NavigationMenuIndicator,
-  NavigationMenuViewport,
-} from './components/ui/navigation-menu';
-import {
-  AlertDialog,
-  AlertDialogPortal,
-  AlertDialogOverlay,
-  AlertDialogTrigger,
-  AlertDialogContent,
-  AlertDialogHeader,
-  AlertDialogFooter,
-  AlertDialogTitle,
-  AlertDialogDescription,
-  AlertDialogAction,
-  AlertDialogCancel,
-} from './components/ui/alert-dialog'
-import { Button } from "./components/ui/button"
-import { Input } from "./components/ui/input"
-import { useState, useEffect, useRef } from "react";
+import { AlertDialog, AlertDialogTrigger, AlertDialogContent, AlertDialogTitle, AlertDialogDescription, AlertDialogCancel } from './components/ui/alert-dialog';
+import { AlertDialogHeader, AlertDialogFooter } from './components/ui/alert-dialog';
+import { Button } from './components/ui/button';
+import { Input } from './components/ui/input';
+
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [text, setText] = useState('');
   const [name, setName] = useState('');
   const [weight, setWeight] = useState('');
-  let [abilities, setAbilities] = useState(['']);
-  let [abilityUrl, setAbilityUrl] = useState(['']);
+  const [abilities, setAbilities] = useState(['']);
+  const [abilityText, setAbilityText] = useState(['']);
   const [imageUrl, setImageUrl] = useState('');
 
   const url = `https://pokeapi.co/api/v2/pokemon/${text.toLowerCase()}`;
-  let firstCharacter = name.charAt(0).toUpperCase();
-  let restOfName = name.slice(1);
-  
-  let numberOfAbilities = 0;
+  const firstCharacter = name.charAt(0).toUpperCase();
+  const restOfName = name.slice(1);
 
-  let i = 0;
-  let j = 0;
-  let k = 0;
-  let id = [''];
-  id.shift();
-  
-  async function fetchData(){
+  async function fetchAbilityData(urlValue, index) {
+    return fetch(urlValue)
+      .then(response => response.json())
+      .then(data => {
+        const descriptions = data.flavor_text_entries.filter(entry => entry.language.name === 'en');
+        const description = descriptions.length > 0 ? descriptions[0].flavor_text : '';
+        setAbilityText(prevAbilityText => [...prevAbilityText, description]);
+      })
+      .catch(error => console.log(error));
+  }
+
+  async function fetchData() {
+    setAbilityText([]);
     fetch(url)
       .then(response => response.json())
       .then(info => {
-        //Setting the image, name and weight of the pokemon
-        setImageUrl(info.sprites.front_default)
-        setName(info.name)
-        setWeight(info.weight)
+        setImageUrl(info.sprites.front_default);
+        setName(info.name);
+        setWeight(info.weight);
 
-        //Cleaning the abilities array
-        numberOfAbilities = info.abilities.length;
-        if(abilities[0] != ''){
-          for(j = 0;j < abilities.length; j++){
-            abilities[j] = '';
-          }
-          abilities.shift();
+        if (abilities[0] !== '') {
+          setAbilities(['']);
         }
 
-        //Cleaning the abilities URL's array
-        if(abilityUrl[0] != ''){
-          console.log('LIMPAR')
-          for(k = 0;k < numberOfAbilities;k++){
-            abilityUrl[k] = '';
-          }
-          abilityUrl.shift();
-        }
+        const abilityUrl = info.abilities.map(ability => ability.ability.url);
 
-        //Adding the URL's of the abilities
-        for(k = 0;k < numberOfAbilities;k++){
-          abilityUrl.push(info.abilities[k].ability.url);
-        }
-        abilityUrl.shift();
-
-        //Adding the names of the abilities
-        for(i = 0; i < numberOfAbilities;i++){
-          const abilityName = info.abilities[i].ability.name;
-          const fullName = abilityName[0].toUpperCase() + abilityName.slice(1);
-//(i == 0 && abilities[0] != ''){
-//}else{
-          abilities.push(fullName);
-//}
-          console.log(fullName)
-        }
-        abilities.shift();
+        Promise.all(abilityUrl.map((url, index) => fetchAbilityData(url, index)))
+          .then(() => {
+            setAbilities(info.abilities.map(ability => ability.ability.name.charAt(0).toUpperCase() + ability.ability.name.slice(1)));
+          })
+          .catch(error => console.log(error));
       })
       .catch(error => console.log(error));
   }
 
   useEffect(() => {
-    console.log(abilityUrl)
-  },[name])
+    // Your useEffect logic here
+  }, []);
 
   return (
     <>
-    <div className="p-6 space-y-6 gap-5">
-      <div className='flex justify-center my-5'>
+      <div className="p-6 space-y-6 gap-5">
+        <div className='flex justify-center my-5'>
           <Input id="pokemonInput" placeholder="Pokemon name" className="border rounded w-1/4" value={text} onChange={(e) => setText(e.target.value)}/>
           <Button className='ml-12' onClick={fetchData}>Pesquisar</Button>
-      </div>
-      <div className="flex justify-center my-5">
-        <img src={imageUrl}id="pokemonImage" className="border rounded min-w-64 min-h-64 max-w-64"/>
-        <div className="pl-5">
-          <h1>Name: {firstCharacter + restOfName}</h1>
-          <h1>Weight: {weight}</h1>
-          <NavigationMenu>
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>Habilidades:</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                <div className="p-5 min-w-[250px]">
-                  {
-                    abilities.map((value,index) => {
-                        return <>
-                        <AlertDialog>
-                          <AlertDialogTrigger>
-                            <Button className='m-1 min-w-[200px]'>{value}</Button><br />
-                          </AlertDialogTrigger>
-                          <AlertDialogContent>
-                            <AlertDialogHeader>
-                              <AlertDialogTitle>{value}</AlertDialogTitle>
-                              <AlertDialogDescription>
-                                {abilityUrl[index]}
-                              </AlertDialogDescription>
-                            </AlertDialogHeader>
-                            <AlertDialogFooter>
-                              <AlertDialogCancel>Voltar</AlertDialogCancel>
-                            </AlertDialogFooter>
-                          </AlertDialogContent>
-                        </AlertDialog>
-                      </>
-                    })
-                  }
+        </div>
+        <div className="flex justify-center my-5">
+          <img src={imageUrl} id="pokemonImage" className="border rounded min-w-64 min-h-64 max-w-64" />
+          <div className="pl-5">
+            <h1>Name: {firstCharacter + restOfName}</h1>
+            <h1>Weight: {weight}</h1>
+            <div className="p-5 min-w-[250px]">
+              {abilities.map((value, index) => (
+                <div key={index}>
+                  <AlertDialog>
+                    <AlertDialogTrigger>
+                      <Button className='m-1 min-w-[200px]'>{value}</Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>{value}</AlertDialogTitle>
+                        <AlertDialogDescription>
+                          {abilityText[index]}
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel>Voltar</AlertDialogCancel>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
                 </div>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <script type="module" src="./src/script.ts"></script>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
